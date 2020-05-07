17248c5e21655cbfd1d0baa140911bdca83dcfec
Kauê Murakami <kauetmurakami@gmail.com>
2020-05-07 18:53:23 -0300

    separando rotas da função de chamada

--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -4,29 +4,27 @@ import 'package:weathersearch/blocs/weather_bloc/weather_bloc.dart';
 import 'package:weathersearch/models/weather.dart';
 import 'package:weathersearch/screens/android/weather_page/weather_search_page.dart';
 import 'package:weathersearch/screens/android/weather_page_detail/weather_detail_page.dart';
-
-class Routes {
-  static const String ROUTE_ROOT = "/";
-  static const String ROUTE_WEATHER_DETAILS = "/weather_details_page";
-
-  static Route<dynamic> routeGenerator(RouteSettings settings) {
-    switch (settings.name) {
-
-      case ROUTE_ROOT :
-        return MaterialPageRoute(
-          builder: (_) => WeatherSearchPage()
-        );
-
-      case ROUTE_WEATHER_DETAILS:
-      final Weather weather = settings.arguments; 
-        return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: BlocProvider.of<WeatherBloc>(context),
-            child: WeatherDetailPage(
-              masterWeather: weather,
+part 'routes.dart';
+class Router{
+  
+    static Route<dynamic> routeGenerator(RouteSettings settings) {
+      switch (settings.name) {
+        case RouteList.ROUTE_ROOT :
+          return MaterialPageRoute(
+            builder: (_) => WeatherSearchPage()
+          );
+  
+        case RouteList.ROUTE_WEATHER_DETAILS:
+        final Weather weather = settings.arguments; 
+          return MaterialPageRoute(
+            builder: (_) => BlocProvider.value(
+              value: BlocProvider.of<WeatherBloc>(context),
+              child: WeatherDetailPage(
+                masterWeather: weather,
+              ),
             ),
-          ),
-        );
+          );
+      }
     }
   }
-}
+  
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,7 @@
 
-class Routes{
+part of 'router.dart';
 
+class RouteList{
   static const String ROUTE_ROOT = "/";
   static const String ROUTE_WEATHER_DETAILS = "/weather_details_page";
 
